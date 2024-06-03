854bc55670b6dfe97c252cff605534492aafaa33
Akshat Awasthi <akshatmawasthi@google.com>
2024-06-03 10:13:26 +0530

    Addition of GCE firewall structure.
    Change-Id: Id4ef87a5f59efd50e6e6367a1723891e114a56a3

---
+++ 03-security/GCE/variables.tf
@@ -0,0 +1,51 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+variable "project_id" {
+  type        = string
+  description = "The ID of the Google Cloud project."
+}
+
+variable "network_name" {
+  type        = string
+  description = "The name (or self-link) of the network to create the firewall rule in."
+}
+
+variable "ingress_rules" {
+  type = list(object({
+    name                    = string
+    description             = optional(string, null)
+    disabled                = optional(bool, null)
+    priority                = optional(number, null)
+    destination_ranges      = optional(list(string), [])
+    source_ranges           = optional(list(string), [])
+    source_tags             = optional(list(string))
+    source_service_accounts = optional(list(string))
+    target_tags             = optional(list(string))
+    target_service_accounts = optional(list(string))
+
+    allow = optional(list(object({
+      protocol = string
+      ports    = optional(list(string))
+    })), [])
+    deny = optional(list(object({
+      protocol = string
+      ports    = optional(list(string))
+    })), [])
+    log_config = optional(object({
+      metadata = string
+    }))
+  }))
+  description = "List of ingress rules. This will be ignored if variable 'rules' is non-empty"
+}
---
+++ configuration/gce-firewall.tfvars
@@ -0,0 +1,18 @@
+project_id   = "your-project-ID"
+network_name = "your-network-name"
+ingress_rules = [
+  {
+    name        = "allow-ssh-custom-ranges"
+    description = "Allow SSH access from specific networks"
+    priority    = 1000
+    source_ranges = [
+      "", # Source ranges such as "192.168.1.0/24" or "10.0.0.0/8"
+      ""
+    ]
+    target_tags = ["ssh-allowed", "https-allowed"]
+    allow = [{
+      protocol = "tcp"
+      ports    = ["22", "443"]
+    }]
+  }
+]
